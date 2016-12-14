5006c2778b5359d1e29358cec0f78fd44652f878
Dylan Josephs <djosephs12@gmail.com>
2016-12-14 16:15:42 -0500

    Added enemy deaths and public health bar
    Lasers should die upon hitting enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+	public float health = 150f;
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+		Projectile missile = collider.gameObject.GetComponent<Projectile> ();
+		if (missile) {
+			print ("missile");
+			health -= missile.GetDamage ();
+			print (health);
+			missile.Hit ();
+			if (health <= 0) {
+				Destroy (gameObject);
+			}
+		
+		
+		}
+		
+		
+		
+	}
+}
