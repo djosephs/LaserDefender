32af854afeff09da5a61b59cafcfe5f8e63085af
Dylan Josephs <djosephs12@gmail.com>
2016-12-15 15:00:51 -0500

    Updates to Blockbreaker:
    -Layering to prevent collisions
    -Multipel shredders

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,7 +3,14 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
+	public GameObject projectile;
+	public float projectileSpeed = 3f;
+	public float firingRate = .5f;
 	public float health = 150f;
+	public float shotsPerSecond = .5f;
+	
+	
+	
 	void OnTriggerEnter2D (Collider2D collider)
 	{
 		Projectile missile = collider.gameObject.GetComponent<Projectile> ();
@@ -22,4 +29,28 @@ public class Enemy : MonoBehaviour
 		
 		
 	}
+	
+	void Fire ()
+	{
+		GameObject beam = Instantiate (projectile, this.transform.position, Quaternion.identity) as GameObject;
+		beam.rigidbody2D.velocity = new Vector3 (0, -1 * projectileSpeed, 0); 
+	}
+	
+	
+	
+	void Update ()
+	{
+		float probability = Time.deltaTime * shotsPerSecond;
+		if (Random.value < probability) {
+			Fire ();	
+		}
+			
+	}
+	
+	
 }
+	
+
+	
+
+
